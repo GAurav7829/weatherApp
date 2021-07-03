0b21aa0300f693a50c9c8c70b67a79d9ecb52ae4
Gaurav Singh <grvsgh7@gmail.com>
2021-07-03 21:01:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [weatherData, setWeatherData] = useState({});
+  const [lat, setLat] = useState(0);
+  const [lon, setLon] = useState(0);
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(position => {
+      setLat(position.coords.latitude);
+      setLon(position.coords.longitude);
+    });
+    axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=5df806a025e90ac22a0bedc34953c5fe`)
+      .then(response => {
+        setWeatherData(response.data);
+        console.log(weatherData);
+      }).catch(error => {
+        console.log(error);
+      });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>City: {weatherData.name}</h1>
+        <h2>Cod: {weatherData.cod}</h2>
+        <h2>Temp: {weatherData.main ? weatherData.main.temp : "no data"}</h2>
+        <h2>Feels Like Temp: {weatherData.main ? weatherData.main.feels_like : "no data"}</h2>
       </header>
     </div>
   );
