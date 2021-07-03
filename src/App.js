72ac5d346f23db64520ceda23d26100a66f0d8eb
Gaurav Singh <grvsgh7@gmail.com>
2021-07-03 21:34:36 +0530

    updated api key

--- src/App.js
+++ src/App.js
@@ -14,16 +14,14 @@ function App() {
     axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=5df806a025e90ac22a0bedc34953c5fe`)
       .then(response => {
         setWeatherData(response.data);
-        console.log(weatherData);
       }).catch(error => {
         console.log(error);
       });
-  }, []);
+  }, [lon, lat]);
   return (
     <div className="App">
       <header className="App-header">
         <h1>City: {weatherData.name}</h1>
-        <h2>Cod: {weatherData.cod}</h2>
         <h2>Temp: {weatherData.main ? weatherData.main.temp : "no data"}</h2>
         <h2>Feels Like Temp: {weatherData.main ? weatherData.main.feels_like : "no data"}</h2>
       </header>
