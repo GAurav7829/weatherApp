db807eb53801be072650800d462ab39adaabd98c
Gaurav Singh <grvsgh7@gmail.com>
2021-07-09 20:12:55 +0530

    updated logic

--- src/App.js
+++ src/App.js
@@ -14,25 +14,18 @@ function App() {
   const [lat, setLat] = useState(0);
   const [lon, setLon] = useState(0);
   const [city, setCity] = useState('');
+  const [error, setError] = useState('');
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(position => {
-      setLat(position.coords.latitude);
-      setLon(position.coords.longitude);
-    });
-    axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${appid}`)
-      .then(response => {
-        setWeatherData(response.data);
-      }).catch(error => {
-        console.log(error);
-      });
+    getGeoLocn();
   }, [lon, lat]);
   const getCityTemp = () => {
     axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${appid}`)
       .then(response => {
         setWeatherData(response.data);
+        setError('');
       }).catch(error => {
         console.log(error);
-        alert('No city Found...')
+        setError('No City Found');
       });
     setCity('');
   }
@@ -44,15 +37,17 @@ function App() {
     axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${appid}`)
       .then(response => {
         setWeatherData(response.data);
+        setError('');
       }).catch(error => {
         console.log(error);
+        setError('Not able to find your location');
       });
   }
   return (
     <div className="App">
       <header className="App-header">
         <Container className="Container" >
-          <div>
+          <div className="offset-md-3 col-md-6">
             <Card>
               <CardBody style={weatherData.main ? weatherData.main.temp > 18 ? { backgroundColor: '#D1AC1D' } : { backgroundColor: '#5E77EE' } : null} >
                 <CardTitle tag="h5">{weatherData.name}<sup>{weatherData.sys ? weatherData.sys.country : ''}</sup></CardTitle>
@@ -77,16 +72,19 @@ function App() {
               </CardBody>
             </Card>
           </div>
-          <div>
+          <div className="offset-md-3 col-md-6">
             <InputGroup>
               <Input placeholder="Enter City Name To Search" value={city} onChange={(e) => { setCity(e.target.value) }} />
               <InputGroupAddon addonType="append">
                 <Button color="info" onClick={getCityTemp}>Search</Button>
               </InputGroupAddon>
               <InputGroupAddon addonType="append">
-                <Button color="link" onClick={getGeoLocn}><img src='/gps_fixed.png' alt='Get Current Location' /></Button>
+                <Button outline color="info" onClick={getGeoLocn}><img src='/gps_fixed.png' alt='Get Current Location' /></Button>
               </InputGroupAddon>
             </InputGroup>
+          </div>
+          <div className="offset-md-3 col-md-6">
+            <div style={{ color: '#FA5C1A' }}>{error}</div>
           </div>
         </Container>
 
